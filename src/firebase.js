e8d2813f6507f9c171bd14656b6def5a91102cdd
Neveen Abbas <neveen.abbas555@gmail.com>
2024-10-31 02:32:48 +0300

    Updated the keys for the firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-require('dotenv').config()
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,15 +10,18 @@ require('dotenv').config()
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: "462949536765",
-  appId: "1:462949536765:web:67fa1539b49e5b264d3f94",
-  measurementId: "G-0NSHXVFQ87",
-  databaseURL: "https://taskvault-886a6-default-rtdb.europe-west1.firebasedatabase.app/",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASURE_ID,
+  databaseURL: import.meta.env.VITE_DB_URL,
 };
+
+
+console.log(import.meta.env.VITE_API_KEY) 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
